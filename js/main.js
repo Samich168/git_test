975730bf08a94e0377b4557e564549426ede39dc
Samich168 <abcdefg2004z@gmail.com>
2022-07-09 16:02:10 +0600

    Jquery

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const hotelSwiper = new Swiper('.hotel-swiper', {
+$(document).ready(function () {
+  const hotelSwiper = new Swiper('.hotel-swiper', {
     // Optional parameters
     loop: true,
   
@@ -8,16 +9,35 @@ const hotelSwiper = new Swiper('.hotel-swiper', {
       prevEl: '.swiper-button--prev',
     },
   
-});
+  });
 
-const reviewSwiper = new Swiper('.review-swiper', {
+  const reviewSwiper = new Swiper('.review-swiper', {
   // Optional parameters
-  loop: true,
+    loop: true,
 
   // Navigation arrows
-  navigation: {
-    nextEl: '.review-button--next',
-    prevEl: '.review-button--prev',
-  },
+    navigation: {
+      nextEl: '.review-button--next',
+      prevEl: '.review-button--prev',
+    },
 
+  });
+
+  var ModalButton = $("[data-toggle=modal_window]");
+  var ModalClose = $("[data-toggle=modal__closer]")
+  ModalButton.on("click", openModal);
+  ModalClose.on("click", closeModal);
+  function openModal(){
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+  function closeModal(event){
+    event.preventDefault()
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
 });
